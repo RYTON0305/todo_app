58e2b00ddac173649f54d57627a32f622699bcda
RYTON <ryton.jiang@gmail.com>
2020-05-07 14:04:43 +0800

    项目初始化及页面搭建

---
+++ src/components/Todo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="title">
+      Todo list
+    </div>
+
+    <div class="main-box">
+      <div class="todo-input">
+        <input type="text" placeholder="请输入Todo" class="input-box" />
+        <button class="input-btn">+</button>
+      </div>
+      <ul class="todo-list">
+        <li class="todo-item"></li>
+      </ul>
+      <div class="todo-footer">
+        <div class="footer-count">一共有0个Todo</div>
+        <div class="footer-clear">清除所有Todo</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo'
+}
+</script>
+
+<style scoped lang="less"></style>
